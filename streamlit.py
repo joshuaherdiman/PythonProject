b2a2d05cca834450e2a43dac1ddf574b6f4e1946
joshuaherdiman <joshuaherdiman@gmail.com>
2025-11-27 21:01:40 +0700

    New Full on Fish Model :D

---
+++ streamlit.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from fastai.vision.all import *
+from requirements.txt import *
+
+st.title("Fish Species Identification")
+st.text("Built by Joshua")
+
+fish_model = load_learner("Joshua's Notebook (9-AM) Project.pkl")
+
+def extract_images(file_name):
+    p = Path(file_name)
+    species_name_parts = p.stem.split("_")
+
+    final_species_name = " "
+    length_parts = len(species_name_parts)-1
+    for i in range(length_parts):
+        final_species_name += species_name_parts[i]
+        if i != length_parts:
+            final_species_name += "_"
+
+    return final_species_name
+
+def predict(image):
+    img = PILImage.create(image)
+    pred_class, pred_idx, outputs = fish_model.predict(img)
+    return pred_class
+
+uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
+if uploaded_file is not None:
+    st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+    st.subheader(f"Prediction Result:")
+
+st.text("Built with Streamlit and Fastai")
